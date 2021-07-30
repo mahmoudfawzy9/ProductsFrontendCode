a0dac532c8e2ce28cd6b35fee87181bb1c056d10
mahmoudfawzy9 <47506233+mahmoudfawzy9@users.noreply.github.com>
2021-07-30 03:24:25 +0200

    The app config
    The app config for user auth

---
+++ my-app-config.ts
@@ -0,0 +1,9 @@
+export default {
+
+    oidc: {
+        clientId: '0oa19ehn9z0x4dItJ5d7',
+        issuer: 'https://dev-54715812.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+}
